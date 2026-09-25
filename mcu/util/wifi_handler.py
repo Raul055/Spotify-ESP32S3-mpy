bdb6243bb468e858b634448f0a98c909dce9e928
Raul055 <raul_rr305@outlook.com>
2026-09-25 02:16:03 -0600

    Update wifi_handler.py
    Wifi handler supports access point for wifi credentials

--- mcu/util/wifi_handler.py
+++ mcu/util/wifi_handler.py
@@ -77,6 +77,14 @@ class wifi_handler:
     # -- Creates an ap portal for wifi credentials
     def create_ap_portal(self):
         try:
+            # Socket was open & ap up
+            if self.s is not None:
+                try:
+                    self.s.close()
+                    self.ap.active(False)
+                except Exception:
+                    pass
+            
             # Creates local server
             self.ap = network.WLAN(network.AP_IF)
             self.ap.active(True)
@@ -84,6 +92,7 @@ class wifi_handler:
 
             # Socket
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.s.bind(('', 80))
             self.s.listen(1)
 
@@ -115,42 +124,69 @@ class wifi_handler:
             return f.read()
     
     # -- Polls when a html request for wifi credentials is done
-    def ap_portal_polling(self):
+    def ap_portal_polling(self, timeout_seconds=180):
+        # Sets 120 seconds for timeout
+        self.s.settimeout(timeout_seconds)
+        
         # Flag
         wifi_credentials_sent = False
-        
-        # While True
-        while not wifi_credentials_sent:
-            # Connection
-            conn, addr = self.s.accept()
-            request = conn.recv(1024).decode()
 
-            # - Gets SSID and Password
-            if "GET /save" in request:
-                # Check params
-                params = request.split("GET /save?")[1].split(" ")[0]
-                kv = dict(p.split("=") for p in params.split("&"))
-                new_ssid = kv["ssid"]
-                new_pass = kv["pass"]
+        try:    
+            # While True
+            while not wifi_credentials_sent:
+                # Connection
+                conn, addr = self.s.accept()
+                request = conn.recv(1024).decode()
 
-                # Update credentials
-                with open(self.credentials) as f:
-                    config = json.load(f)
-                config["wifi"]["ssid"] = new_ssid
-                config["wifi"]["password"] = new_pass
-                with open(self.credentials, "w") as f:
-                    json.dump(config, f)
+                # - Gets SSID and Password
+                if "GET /save" in request:
+                    # Check params
+                    params = request.split("GET /save?")[1].split(" ")[0]
+                    kv = dict(p.split("=") for p in params.split("&"))
+                    new_ssid = kv["ssid"]
+                    new_pass = kv["pass"]
 
-                # Close connection
-                conn.send("HTTP/1.1 200 OK\r\n\r\nSaved. Rebooting...")
-                conn.close()
-                return True
-            
-            # - Loads page
-            else:
-                html = self.load_page("html/ap_page.html")
-                conn.send("HTTP/1.1 200 OK\r\n\r\n" + html)
-                conn.close()
+                    # Update credentials
+                    with open(self.credentials) as f:
+                        config = json.load(f)
+                    config["wifi"]["ssid"] = new_ssid
+                    config["wifi"]["password"] = new_pass
+                    with open(self.credentials, "w") as f:
+                        json.dump(config, f)
+
+                    # Uodates ssid and password
+                    self.ssid = new_ssid
+                    self.password = new_pass
+                    
+                    # Close connection
+                    conn.send("HTTP/1.1 200 OK\r\n\r\nSaved. Rebooting...")
+                    conn.close()
+
+                    # Updates flag
+                    wifi_credentials_sent = True
+
+                    return True
+                
+                # - Loads page
+                else:
+                    html = self.load_page("html/ap_page.html")
+                    conn.send("HTTP/1.1 200 OK\r\n\r\n" + html)
+                    conn.close()
+
+            return False
+
+        # Something went wrong, error
+        except OSError as e:
+            self.error_handler(e)
+            return False
+
+        # Close if something happens
+        finally:
+            try:
+                self.s.close()
+                self.ap.active(False)
+            except Exception:
+                pass
 
 if __name__ == "__main__":
     
